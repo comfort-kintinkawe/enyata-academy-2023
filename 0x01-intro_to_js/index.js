d00f10a9f87c93aa4a24b628e1fc117a1e9f9c9e
comfort-kintinkawe <comfortkintinkawe75@gmail.com>
2023-05-25 23:53:44 +0000

    javascript assignment

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,20 @@
+let myname = "comfort";
+
+
+console.log(typeof myname);
+
+myname -"comfort";
+
+let car = {
+    brand: "toyotacamry",
+    fueltype: "petrol",
+    seatingcapacity: "5",
+    fueltankcapacity: "50.0",
+    bodytype: "sedan",
+    color: "black",
+    enginedisplacement: "2487",
+    isManuel: false,
+    feature: "radio,bluetooth",
+    year: "2022",
+    
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,13 @@
+
+const favoriteNumber = 6
+
+const halfoffavNum =  favoriteNumber / 3
+
+let isaacFavorite = 13;
+
+isaacFavorite = isaacFavorite - favoriteNumber 
+
+isaacFavorite = 26 * isaacFavorite;
+
+isaacFavorite = isaacFavorite % 3;
+
